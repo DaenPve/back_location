f5e4d41beb334bf2eb1df9f3b6074e38cac2c5f7
DaenPve <17030169@itcelaya.edu.mx>
2024-07-25 17:52:32 -0600

    update app.js and structure

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
-app.use(cors())
-app.use(require('./routes/rutas'))
+const app = express();
+const server = http.createServer(app);
 
+const wss = new WebSocket.Server({ server });
 
-app.listen('3000')
-console.log('Servidor ON')
+app.use(bodyParser.json());
+
+app.get('/api/status', (req, res) => {
+  res.json({ status: 'Servidor en funcionamiento' });
+});
+
+app.post('/api/endpoint', (req, res) => {
+  console.log('Mensaje recibido en el endpoint HTTP:', req.body);
+  
+  res.json({ status: 'Mensaje recibido' });
+});
+
+wss.on('connection', (ws) => {
+  console.log('Cliente WebSocket conectado');
+
+  ws.on('message', (message) => {
+    console.log('Mensaje recibido:', message.toString());
+    
+    ws.send('Mensaje recibido');
+
+  });
+
+  ws.on('close', () => {
+    console.log('Cliente WebSocket desconectado');
+  });
+});
+
+server.listen(8080, () => {
+  console.log('Servidor WebSocket y HTTP en funcionamiento en http://localhost:8080');
+});
--- routes/rutas.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express')
-const router = Router();
-const { location } = require('../controllers/index.controller')
-
-/* Obtener ubicacion enviada */
-router.post('/location', location);
-module.exports = router
